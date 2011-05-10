6064668dd6abdeba406e8dac1b4f4f3216db987d
foolegg <sd.foolegg@gmail.com>
2011-05-10 10:34:52 +0800

    tooltip done

---
+++ skeleton/utils/config.py
@@ -0,0 +1,15 @@
+import os
+
+def check_path( path_list ) :
+    result = ""
+    for path in path_list :
+        if os.path.exists( path ) :
+            result = path
+            break
+    return result
+
+#runtime_path = [ os.path.expanduser( "~/.config/cim/data/new_formated" ), "/opt/cim/data/new_formated", "../../data/new_formated", os.path.expanduser( "~/workspace/cim/" ) ]
+
+sysdict_path = [ os.path.expanduser( "~/.config/cim/data/new_formated" ), "/opt/cim/data/new_formated", "../../data/new_formated", os.path.expanduser( "~/workspace/cim/" ) ]
+
+qml_qwert_path = [ os.path.expanduser( "~/.config/cim/skeleton/qwert/qml" ), "/opt/cim/skeleton/qwert/qml", "./qml/qwert.qml" ]
