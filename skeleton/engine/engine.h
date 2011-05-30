9c6d58e057e93899cbee125261c218856d377b7c
liang <sd.foolegg@gmail.com>
2011-05-30 15:16:32 +0800

    qt engine done, start merge

--- skeleton/engine/engine.h
+++ skeleton/engine/engine.h
@@ -2,20 +2,34 @@
 #define IMENGINE_H
 
 #include <QObject>
+#include <QList>
+#include <QString>
+#include <QStringList>
 #include <QSet>
 #include <QFile>
 #include <QTextStream>
 
 #include "../lookup/lookup.h"
 
+namespace engine {
+
+typedef QPair<QString, QString> KeyPair ;
+typedef QPair<QChar, qreal> WordPair ;
+typedef QPair<KeyPair, WordPair> SelectedPair ;
+
+//typedef QPair<QStringList, QStringList> KeyPair ;
+//typedef QPair< QString, QList<qreal> > WordPair ;
+
 class IMEngine : public QObject {
     Q_OBJECT
 public:
     lookup::Lookup lookup ;
-    const lookup::Candidate* candidate ;
+    QList<SelectedPair> selected ;
+    QString selectedWord ;
     int pageIndex ;
+    const lookup::Candidate* candidate ;
 
-    inline IMEngine( QObject* parent = NULL ) : QObject( parent ), lookup() { this->pageIndex = 0 ; this->candidate = NULL ; }
+    inline IMEngine( QObject* parent = NULL ) : QObject( parent ), lookup(), selected(), selectedWord() { this->pageIndex = 0 ; this->candidate = NULL ; }
     
     Q_INVOKABLE inline void load( const QString& path ) {
         QFile file( path ) ;
@@ -93,36 +107,89 @@ public:
             return "" ;
     }
     Q_INVOKABLE inline void select( int index ) {
-        //candIndex = self.pageIndex * self.pageLength + index
-        //cand = self.lookup[self.mode].getCand( candIndex )
-        //invaildCode = self.getInvaildCode( index )
-        //if cand :
-            //#print candIndex
-            //key, word, freq, preeditString, candStartIndex = cand 
-            //#print key, word, freq, preeditString, candStartIndex
-            //freqArg = -1
-            //if len( self.selected ) <= 0 :
-                //halfIndex = ( candStartIndex + candIndex ) / 2
-                //halfCand = self.lookup[self.mode].getCand( halfIndex )
-                //halfKey, halfWord, halfFreq, halfPreeditString, halfCandStartIndex = halfCand
-                //#print halfKey, halfWord, halfFreq, halfPreeditString, halfCandStartIndex
-                //freqArg = halfFreq + 1
-            //keys = key.split( "'" )
-            //preeditStrings = preeditString.split( "'" )
-            //for i in range( len( word ) ) :
-                //self.selected.append( [ keys[i], word[i], -1, preeditStrings[i] ] )
-            //self.selectedWord += word
-            //self.selected[-1][2] = freqArg
-            //#print self.selected
-            //self.lookup[self.mode].clear()
-            //self.pageIndex = 0
-            //if len( invaildCode ) > 0 :
-                //for c in invaildCode :
-                    //self.lookup[self.mode].append( c )
-        //else :
-            //pass
+        index = this->pageIndex * 5 + index ;
+        const lookup::Candidate* candidate = this->lookup.getCand( index ) ;
+
+        if ( candidate ) {
+            qreal freq = -0x1000 ;
+            if ( this->selected.isEmpty() ) {
+                int halfIndex = ( candidate->second + index ) / 2 ;
+                const lookup::Candidate* candidate = this->lookup.getCand( halfIndex ) ;
+                freq = lookup::get_freq( candidate ) ;
+            }
+            
+            const QString* word = lookup::get_word( candidate ) ;
+            if ( word->length() > 1 ) {
+                QStringList key = lookup::get_key( candidate )->split( "'" ) ;
+                QStringList preedit = lookup::get_preedit( candidate )->split( "'" ) ;
+                for ( int i = 0 ; i < word->length() ; i++ )
+                    this->selected.append( SelectedPair( KeyPair( key.at(i), preedit.at(i) ), WordPair( word->at(i), -0x1000 ) ) ) ;
+            }
+            else {
+                const QString* key = lookup::get_key( candidate ) ;
+                const QString* preedit = lookup::get_preedit( candidate ) ;
+                this->selected.append( SelectedPair( KeyPair( *key, *preedit ), WordPair( word->at(0), -0x1000 ) ) ) ;
+            }
+            this->selectedWord.append( *word ) ;
+            this->selected.last().second.second = freq ;
+
+            this->candidate = candidate ;
+            if ( this->getInvaildCodeLength() > 0 ) {
+                QString code( this->getInvaildCode() ) ;
+                this->lookup.reset() ;
+                this->lookup.setCode( code ) ;
+                this->pageIndex = 0 ;
+            }
+            else {
+                this->lookup.reset() ;
+                this->pageIndex = 0 ;
+            }
+        }
+        //else ;
     }
+    Q_INVOKABLE inline void cancel() {
+        if ( !this->selected.isEmpty() ) {
+            QString code = this->selected.last().first.second ;
+            this->selected.removeLast() ;
+            this->selectedWord.chop(1) ;
+
+            code.append( this->lookup.spliter.code ) ;
+            this->lookup.reset() ;
+            this->lookup.setCode( code ) ;
+            this->pageIndex = 0 ;
+        }
+    }
+    Q_INVOKABLE inline void reset() {
+        this->lookup.reset() ;
+        this->selected.clear() ;
+        this->selectedWord.clear() ;
+        this->pageIndex = 0 ;
+    }
+    Q_INVOKABLE inline void appendCode( QChar code ) {
+        this->lookup.appendCode( code ) ;
+        this->pageIndex = 0 ;
+    }
+    Q_INVOKABLE inline void popCode() {
+        this->lookup.popCode() ;
+        this->pageIndex = 0 ;
+    }
+    Q_INVOKABLE inline void commit() {
+        if ( !this->selected.isEmpty() ) {
+            QStringList key ;
+            for ( int i = 0 ; i < this->selected.length() ; i++ )
+                key.append( this->selected.at(i).first.first ) ;
+            QString k = key.join( "'" ) ;
+            //qreal freq = this->selected.last().second.second ;
+            //this->lookup.update( k, this->selectedWord, freq ) ;
+            this->reset() ;
+        }
+    }
+    Q_INVOKABLE inline void setMode( int index ) {
+    }
+
 } ;
+
+}
 
 #endif
 
